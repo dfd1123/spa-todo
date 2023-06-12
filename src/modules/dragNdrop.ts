a65ff1a2ad47d28bdc8334f42e83f2af7f521d27
dfd1123 <dfd1123@naver.com>
2023-06-12 12:57:25 +0900

    dragNdrop 모듈 소스 정리 및 기능 수정

--- src/modules/dragNdrop.ts
+++ src/modules/dragNdrop.ts
@@ -8,13 +8,12 @@ export default class DragDrop {
   private listSelector: string;
   private $listContEl: HTMLElement;
   private dragListArr: HTMLElement[];
-  protected isDragging = false;
+  private isDragging = false;
   private $draggedItem: HTMLElement;
   private drageedIndex: number = -1;
   private dragTimeoutId: number = -1;
-  private dragReadyTimeoutId: number = -1;
+  private mousedownTimeoutId: number = -1;
   private $mirrorElement: HTMLElement = null;
-  private isMoved = false;
   private translateX = 0;
   private initialY = 0;
   private onListChange?: (newIdList: string[]) => void;
@@ -44,7 +43,7 @@ export default class DragDrop {
     window.onkeydown = (e) => this.handleKeyEscape(e);
 
     this.dragListArr.forEach((item) => {
-      item.onmousedown = (e) => this.handleDragStart(e);
+      item.onmousedown = (e) => this.handleMouseDown(e);
       item.onmouseup = (e) => this.handleDragEnd(e);
     });
   }
@@ -56,12 +55,19 @@ export default class DragDrop {
     target && target.classList.add(className);
   }
 
+  setDragItemInfo(event: MouseEvent) {
+    this.$draggedItem = this.getClosetDraggableTarget(event);
+    this.drageedIndex = Array.from(this.$listContEl.children).indexOf(
+      this.$draggedItem
+    );
+  }
+
   setDragTargetPosition(target: HTMLElement) {
     const targetIndex = this.dragListArr.indexOf(target);
 
     if (targetIndex > -1 && this.$draggedItem && target !== this.$draggedItem) {
       this.$listContEl?.removeChild(this.$draggedItem);
-      if (targetIndex < this.drageedIndex) {
+      if (targetIndex <= this.drageedIndex) {
         target.before(this.$draggedItem);
         this.drageedIndex = targetIndex - 1;
       } else if (targetIndex > this.drageedIndex) {
@@ -78,36 +84,42 @@ export default class DragDrop {
     }
   }
 
-  handleKeyEscape(event: KeyboardEvent) {
-    if (this.isDragging && event.key === 'Escape') {
-      this.$draggedItem = null;
-      this.isDragging = false;
-      this.drageedIndex = null;
-
-      if (this.$mirrorElement?.parentNode) {
-        this.$mirrorElement.parentNode.removeChild(this.$mirrorElement);
-      }
-    }
-  }
-
-  handleDragStart(event: MouseEvent) {
-    this.isMoved = false;
-
-    const target = (event.target as HTMLElement).closest(
+  getClosetDraggableTarget(event: MouseEvent) {
+    return (event.target as HTMLElement).closest(
       `.${DRAG_ITEM_CLASSNAME}`
     ) as HTMLElement;
+  }
 
-    if (!target.draggable) return;
+  deleteMirrorElements() {
+    if (this.$mirrorElement?.parentNode) {
+      this.$mirrorElement.parentNode.removeChild(this.$mirrorElement);
+    }
 
-    this.$draggedItem = event.currentTarget as HTMLElement;
+    const els = [...document.body.querySelectorAll('.todo-item.mirror')];
+    els.forEach((el) => el.remove());
+  }
 
-    target.classList.add(DRAGGING_CLASSNAME);
-    this.drageedIndex = Array.from(this.$listContEl.children).indexOf(
-      this.$draggedItem
-    );
+  resetDragStatus() {
+    this.$draggedItem = null;
+    this.isDragging = false;
+    this.drageedIndex = null;
 
-    this.isDragging = true;
+    this.dragListArr.forEach((el) => {
+      el.classList.remove(DRAG_TARGET_CLASSNAME);
+      el.classList.remove(DRAGGING_CLASSNAME);
+    });
 
+    this.deleteMirrorElements();
+  }
+
+  onListChangeTrigger() {
+    this.onListChange &&
+      this.onListChange(
+        this.dragListArr.map((el) => el.dataset.dragId).filter((id) => !!id)
+      );
+  }
+
+  mirrorElementCreate() {
     this.$mirrorElement = this.$draggedItem.cloneNode(true) as HTMLElement;
     this.$mirrorElement.classList.add(DRAG_MIRROR_CLASSNAME);
 
@@ -120,12 +132,34 @@ export default class DragDrop {
     this.$mirrorElement.style.width = `${rect.width}px`;
     this.$mirrorElement.style.pointerEvents = 'none';
 
-    this.$listContEl.appendChild(this.$mirrorElement);
+    document.body.appendChild(this.$mirrorElement);
+  }
+
+  handleKeyEscape(event: KeyboardEvent) {
+    if (this.isDragging && event.key === 'Escape') {
+      this.resetDragStatus();
+    }
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    // 마우스 오른쪽 버튼 클릭 시 무시
+    if (event.button === 2) return;
+
+    const target = this.getClosetDraggableTarget(event);
+
+    // 드래그할 수 없는 아이템일 경우 무시
+    if (!target.draggable) return;
+
+    this.mousedownTimeoutId = window.setTimeout(() => {
+      this.setDragItemInfo(event);
+      target.classList.add(DRAGGING_CLASSNAME);
+      this.mirrorElementCreate();
+
+      this.isDragging = true;
+    }, 150);
   }
 
   handleDragMove(event: MouseEvent) {
-    this.isMoved = true;
-
     if (this.isDragging) {
       event.preventDefault();
 
@@ -135,16 +169,17 @@ export default class DragDrop {
         event.clientY - this.initialY
       }px)`;
 
-      const target = (event.target as HTMLElement).closest(
-        `.${DRAG_ITEM_CLASSNAME}`
-      ) as HTMLElement;
+      const target = this.getClosetDraggableTarget(event);
 
       this.targetAddClass(target, DRAG_TARGET_CLASSNAME);
 
       window.clearTimeout(this.dragTimeoutId);
       this.dragTimeoutId = window.setTimeout(() => {
         this.setDragTargetPosition(target);
+        window.clearTimeout(this.dragTimeoutId);
       }, 1000);
+    } else {
+      window.clearTimeout(this.mousedownTimeoutId);
     }
   }
 
@@ -152,37 +187,24 @@ export default class DragDrop {
     if (this.isDragging) {
       event.preventDefault();
 
-      const target = (event.target as HTMLElement).closest(
-        `.${DRAG_ITEM_CLASSNAME}`
-      ) as HTMLElement;
+      const target = this.getClosetDraggableTarget(event);
 
       this.setDragTargetPosition(target);
-
-      this.onListChange &&
-        this.onListChange(this.dragListArr.map((el) => el.dataset.dragId));
+      this.resetDragStatus();
+      this.onListChangeTrigger();
     }
+
+    window.clearTimeout(this.mousedownTimeoutId);
   }
 
   handleWindowDragEnd(event: MouseEvent) {
-    if (this.isDragging && this.isMoved) {
+    if (this.isDragging) {
       event.preventDefault();
 
-      this.isDragging = false;
-      this.isMoved = false;
-      this.$draggedItem = null;
-      this.drageedIndex = null;
-
-      if (this.$mirrorElement?.parentNode) {
-        this.dragListArr.forEach((el) => {
-          el.classList.remove(DRAG_TARGET_CLASSNAME);
-          el.classList.remove(DRAGGING_CLASSNAME);
-        });
-
-        this.$mirrorElement.parentNode.removeChild(this.$mirrorElement);
-      }
-
-      this.onListChange &&
-        this.onListChange(this.dragListArr.map((el) => el.dataset.dragId));
+      this.resetDragStatus();
+      this.onListChangeTrigger();
     }
+
+    window.clearTimeout(this.mousedownTimeoutId);
   }
 }
